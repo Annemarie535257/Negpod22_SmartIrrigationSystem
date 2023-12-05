e8c05ef27fce1febeede19260f247d1aa7dcee6b
Annemarie535257 <a.twagiraye@alustudent.com>
2023-12-05 06:55:22 -0800

    done

--- introduction_and_guidelines.py
+++ introduction_and_guidelines.py
@@ -3,5 +3,5 @@ introduction_and_guidelines():
     print("2.This application helps you manage plant irrigation.")
     print("3.Please follow the guidelines for effective plant care.")
     print("4.If you are a first time user go to the sign up button) 
-    print("5. if you are a returning user go to the login button")
+    print("5.if you are a returning user go to the login button")
 introduction_and_guidelines()
