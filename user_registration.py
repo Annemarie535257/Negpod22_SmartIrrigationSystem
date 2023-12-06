5f63db60b25a33ec7a5d551b8e60bc3d2991029f
Annemarie535257 <a.twagiraye@alustudent.com>
2023-12-06 12:05:56 -0800

    done

--- user_registration.py
+++ user_registration.py
@@ -52,18 +52,22 @@ def login():
         print("Invalid username or password.")
 
 #function to view the users in the database
-def view_users():
-    with sqlite3.connect(DATABASE_NAME) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        rows = cursor.fetchall()
-        if not rows:
-            print("No users found.")
-        else:
-            print("Users:")
-            for row in rows:
-                print(f"ID: {row[0]}, Username: {row[1]}, Email: {row[3]}")
 
+def view_signup_details():
+    conn = sqlite3.connect("user_data.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users")
+    signup_details = cursor.fetchall()
+
+    conn.close()
+
+    if signup_details:
+        print("Signup Details:")
+        for user in signup_details:
+            print(f"User ID: {user[0]}, Username: {user[1]}, Email: {user[3]}")
+    else:
+        print("No signup details found.")
 
 
 def main():
@@ -81,7 +85,7 @@ def main():
     elif choice == "2":
         login()
     elif choice == "3":
-        view_users()
+        view_signup_details()
     else:
         print("Invalid choice. Please enter either '1' for Sign Up or '2' for Login.")
 
