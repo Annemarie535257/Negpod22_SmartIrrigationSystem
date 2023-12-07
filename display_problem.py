5666cbb694fb7cb1c19fcf02347e7ababbb96535
Annemarie535257 <a.twagiraye@alustudent.com>
2023-12-07 09:55:18 -0800

    done

--- display_problem.py
+++ display_problem.py
@@ -61,17 +61,6 @@ class IrrigationSystem:
     def needs_irrigation(self, moisture_level):
         return moisture_level < self.threshold
 
-def display_soil_moisture(sensor, irrigation_system):
-    sensor.measure_moisture()
-    moisture_level = sensor.get_moisture_level()
-    need_irrigation = irrigation_system.needs_irrigation(moisture_level)
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Location: {sensor.location}")
-    print(f"Timestamp: {timestamp}")
-    print(f"Soil Moisture Level: {moisture_level:.2f}%")
-
-
 if __name__ == "__main__":
     sensor1 = SoilMoistureSensor(location="Fields")
     irrigation_system1 = IrrigationSystem(threshold=5)
