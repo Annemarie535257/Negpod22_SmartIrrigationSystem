f97b43aec6982f1ad017c90b792710048df4e22a
Annemarie535257 <a.twagiraye@alustudent.com>
2023-12-07 09:20:37 -0800

    done

--- display_problem.py
+++ display_problem.py
@@ -7,14 +7,13 @@ class SoilMoistureSensor:
         self.moisture_level = 0  # Initialize moisture level
 
     def measure_moisture(self):
-        # Simulate measuring soil moisture (replace with actual sensor readings)
-        self.moisture_level = random.uniform(0, 100)
+        self.moisture_level = random.uniform(0, 10)
 
     def get_moisture_level(self):
         return self.moisture_level
 
 class IrrigationSystem:
-    def __init__(self, threshold=30):
+    def __init__(self, threshold=5):
         self.threshold = threshold  # Moisture level below which irrigation is needed
 
     def needs_irrigation(self, moisture_level):
@@ -29,20 +28,4 @@ def display_soil_moisture(sensor, irrigation_system):
     print(f"Location: {sensor.location}")
     print(f"Timestamp: {timestamp}")
     print(f"Soil Moisture Level: {moisture_level:.2f}%")
-
-    if need_irrigation:
-        print("Irrigation is needed!\n")
-    else:
-        print("No irrigation needed.\n")
-
-# Example of usage
-if __name__ == "__main__":
-    # Create a soil moisture sensor for a specific location
-    sensor1 = SoilMoistureSensor(location="Garden")
-
-    # Create an irrigation system with a moisture threshold
-    irrigation_system1 = IrrigationSystem(threshold=30)
-
-    # Display soil moisture information
-    display_soil_moisture(sensor1, irrigation_system1)
 
