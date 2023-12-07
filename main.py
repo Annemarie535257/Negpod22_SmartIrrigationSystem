e4ec0a4d1db8eb2e6e6b63136fbe939def7cd671
Diana-codes <dianaruzindana8@gmail.com>
2023-12-07 14:34:49 -0800

    main file commits

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ from display_problem import SoilMoistureSensor, IrrigationSystem
 from display_problem import display_soil_moisture 
 from user_registration import initialize_database, sign_up, login
 from irrigation_system import menu, start_irrigation, stop_irrigation, adjust_settings, check_system_status
+def main():
+    print("WELCOME TO THE UNIQUE SMART IRRIGATION SYSTEM")
+
+    # Initialize the user database
+    initialize_database()
+        # Create instances of SoilMoistureSensor and IrrigationSystem
+    sensor = SoilMoistureSensor(location="Fields")
+    irrigation_system = IrrigationSystem(threshold=5)
