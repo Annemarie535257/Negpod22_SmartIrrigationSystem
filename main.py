e64c43974534abeeed0ad2a38b77c163280fe1ea
Diana-codes <dianaruzindana8@gmail.com>
2023-12-07 15:42:03 -0800

    main file commits

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import time 
 import random
 from getpass import getpass
-from display_problem import SoilMoistureSensor, IrrigationSystem
+from moisture_check import SoilMoistureSensor, IrrigationSystem
 from display_problem import display_soil_moisture 
 from user_registration import initialize_database, sign_up, login
 from irrigation_system import menu, start_irrigation, stop_irrigation, adjust_settings, check_system_status
