b80ea0c5e309993505d6fc7df5c6f865b6f026f6
Diana-codes <dianaruzindana8@gmail.com>
2023-12-07 23:43:08 -0800

    main file commits

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from getpass import getpass
 from moisture_check import SoilMoistureSensor, IrrigationSystem
 from moisture_check import display_soil_moisture
 from user_registration import initialize_database, sign_up, login
-from irrigation_system import menu, start_irrigation, stop_irrigation, adjust_settings, check_system_status
+from irrigation import menu, start_irrigation, stop_irrigation, adjust_settings, check_system_status
 
 def main():
     print("WELCOME TO THE UNIQUE SMART IRRIGATION SYSTEM")
